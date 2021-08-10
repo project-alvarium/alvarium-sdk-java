e6bbca8c1f407b555c18cd23f88fa3848dc5bf1c
Karim Elghamry <karimelghamry@gmail.com>
2021-08-10 05:16:02 -0700

    adding maven support to the project
    * add maven build folder and ecplipse files to .gitignore
    
    * generate pom.xml for maven
    
    * add the Sdk's basic method signatures
    
    * add a dummy test for the Sdk class
    
    Fix #7
    
    Signed-off-by: Karim Elghamry <karimelghamry@gmail.com>

---
+++ src/test/java/com/alvarium/sdk/SdkTest.java
@@ -0,0 +1,12 @@
+package com.alvarium.sdk;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SdkTest {
+  @Test
+  public void shouldAssertTrue() {
+    assertTrue(true);
+  }
+}
