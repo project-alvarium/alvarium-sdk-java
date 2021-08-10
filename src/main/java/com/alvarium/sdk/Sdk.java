014e1add63c706978cd800e5023b05b2666f28e2
Karim Elghamry <karimelghamry@gmail.com>
2021-08-10 05:16:02 -0700

    adding maven support to the project
    * add maven build folder and ecplipse files to .gitignore
    
    * generate pom.xml for maven
    
    * add the Sdk's basic method signatures
    
    * add a dummy test for the Sdk class
    
    Fix #7
    
    Signed-off-by: Karim Elghamry <karimelghamry@gmail.com>

---
+++ src/main/java/com/alvarium/sdk/Sdk.java
@@ -0,0 +1,22 @@
+package com.alvarium.sdk;
+
+public class Sdk {
+  Sdk() {
+    this.init();
+  }
+
+  private void init() {
+  }
+
+  public void create() {
+  }
+
+  public void mutate() {
+  }
+
+  public void transit() {
+  }
+
+  public void dispose() {
+  }
+}
