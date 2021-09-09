c46c08054ed03cc48be42974a949537f5d12c251
Karim Elghamry <karimelghamry@gmail.com>
2021-09-09 01:06:33 -0700

    Enable sdk configuration driven customization
    * Override enum literals serialization to match lowercase
      implemention of the go sdk
    * Add HashInfo as a Java bean
    * Add SdkInfo as a Java bean
    * Implement unit tests
    
    Fix #38 & Fix #42
    
    Signed-off-by: Karim Elghamry <karimelghamry@gmail.com>

--- src/main/java/com/alvarium/SdkAction.java
+++ src/main/java/com/alvarium/SdkAction.java
@@ -1,8 +1,15 @@
 package com.alvarium;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * An identifier for the operations performed by the sdk
  */
 public enum SdkAction {
-  CREATE, MUTATE, TRANSIT  
+  @SerializedName(value = "create")
+  CREATE,
+  @SerializedName(value = "mutate")
+  MUTATE,
+  @SerializedName(value = "transit")
+  TRANSIT;
 }
